4497f8b33de72d35dbdd3ede2b976ac40fcbd254
Kyle Anthony Perez <kperez@lanl.gov>
2021-04-19 08:45:49 -0600

    Add new file

---
+++ simp_add.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int add_nums( int a, int b ) {
+
+
+	return a + b;
+
+
+}//end add_nums
+
+
+
+
+int main () {
+
+
+	int a = 1;
+	int b = 2;
+	
+	int c = add_nums(a,b);
+
+	printf("Sum of numbers: %f\n", c);
+
+
+
+}//end main
